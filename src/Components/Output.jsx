a069450ec5cea7ba5efc877623592729cdba1b06
arhaamwanii <arhamwani765@gmail.com>
2023-12-16 05:39:08 +0530

    .

--- src/Components/Output.jsx
+++ src/Components/Output.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { CheckCircle, X } from 'lucide-react'
 
-export function SuccessBanner() {
+export function SuccessBanner() {  
+    const height = {
+      height: '100px'
+    }
   return (
     <>
-      <div className="rounded-md border-l-4 border-green-500 bg-green-100 p-4">
+      <div style={height} className="rounded-md border-l-4 border-green-500 bg-green-100 p-4">
         
-        <div className="flex items-center justify-between space-x-4">
-          
+        <div className="flex  items-center justify-between space-x-4">
+          <h4>Response:</h4>
              <div>
             <p className="text-sm font-medium text-green-600">
               This is some informational text that you can use to show some success content
             </p>
-          </div><div>
+          </div>
+          <div>
             <CheckCircle className="h-6 w-6 text-green-600" />
           </div>
        
         </div>
       </div>
     </>
+
+
   )
 }
