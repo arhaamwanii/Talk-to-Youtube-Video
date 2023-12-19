02cd562954ada25c5f4f4f02e0e5b6b23424709b
arhaamwanii <arhamwani765@gmail.com>
2023-12-20 02:57:02 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -101,3 +101,4 @@ export default App
 //it is true that we need to do this to make money
 
 // wahi jahan
+//in the end we all die dont we so we are going to make it to the other end if we dont we will have to it is this constant balttle with the inner self and the battle with the it is this notion that we are going to die that gives life 
